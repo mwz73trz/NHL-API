01b4ad6c7cf033850b4383cf0a03ffd0d133205f
mwz73trz <michaelzura@bellsouth.net>
2021-11-17 19:04:10 -0500

    react set/login set

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import UserContext from "./contexts/UserContext";
+
+class App extends Component {
+  state = {
+    user: null
+  }
+
+  updateUser = (newUserData) => {
+    this.setState({ user: newUserData })
+  }
+
+  renderLoginPage = (routeProps) => {
+    return <LoginPage {...routeProps} completeLogin={this.updateUser} />
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <Router>
+        <UserContext.Provider value={this.state.user}>
+        <div>
+          <Route path="/" exact component={} />
+          <Route path="/login" exact render={this.renderLoginPage} />
+        </div>
+        </UserContext.Provider>
+      </Router>
+    </div>
+  );
+  }
+}
+
+export default App;
